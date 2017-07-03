6675306e24900bad6e38b99e47241b7c9e7b87c1
lihengming <89921218@qq.com>
2017-07-03 14:10:35 +0800

    add > thymeleaf web seed example

--- src/main/java/com/company/project/configurer/WebMvcConfigurer.java
+++ src/main/java/com/company/project/configurer/WebMvcConfigurer.java
@@ -15,10 +15,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -61,34 +62,40 @@ public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
         exceptionResolvers.add(new HandlerExceptionResolver() {
             public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
+                ModelAndView mv = new ModelAndView();
                 Result result = new Result();
                 if (handler instanceof HandlerMethod) {
                     HandlerMethod handlerMethod = (HandlerMethod) handler;
+                    if (null != handlerMethod.getBean().getClass().getAnnotation(RestController.class)
+                            || null != handlerMethod.getMethodAnnotation(ResponseBody.class)) {//返回JSON格式
 
-                    if (e instanceof ServiceException) {//业务失败的异常，如“账号或密码错误”
-                        result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
-                        logger.info(e.getMessage());
-                    } else {
-                        result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
-                        String message = String.format("接口 [%s] 出现异常，方法：%s.%s，异常摘要：%s",
-                                request.getRequestURI(),
-                                handlerMethod.getBean().getClass().getName(),
-                                handlerMethod.getMethod().getName(),
-                                e.getMessage());
-                        logger.error(message, e);
+                        if (e instanceof ServiceException) {//业务失败的异常，如“账号或密码错误”
+                            result.setCode(ResultCode.FAIL).setMessage(e.getMessage());
+                            logger.info(e.getMessage());
+                        } else {
+                            result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage("接口 [" + request.getRequestURI() + "] 内部错误，请联系管理员");
+                            String message = String.format("接口 [%s] 出现异常，方法：%s.%s，异常摘要：%s",
+                                    request.getRequestURI(),
+                                    handlerMethod.getBean().getClass().getName(),
+                                    handlerMethod.getMethod().getName(),
+                                    e.getMessage());
+                            logger.error(message, e);
+                        }
+                        responseResult(response, result);
+                    } else {//普通页面
+                        if (response.getStatus() == 404) {
+                            mv.setViewName("error/404");
+                        } else {
+                            mv.setViewName("error/500");
+                            logger.error(e.getMessage(), e);
+                        }
                     }
                 } else {
-                    if (e instanceof NoHandlerFoundException) {
-                        result.setCode(ResultCode.NOT_FOUND).setMessage("接口 [" + request.getRequestURI() + "] 不存在");
-                    } else {
-                        result.setCode(ResultCode.INTERNAL_SERVER_ERROR).setMessage(e.getMessage());
-                        logger.error(e.getMessage(), e);
-                    }
-                }
-                responseResult(response, result);
-                return new ModelAndView();
-            }
+                    logger.error(e.getMessage(), e);
 
+                }
+                return mv;
+            }
         });
     }
 
--- src/main/java/com/company/project/web/UserController.java
+++ src/main/java/com/company/project/web/UserController.java
@@ -1,8 +1,8 @@
-package ${basePackage}.web;
-import ${basePackage}.core.Result;
-import ${basePackage}.core.ResultGenerator;
-import ${basePackage}.model.${modelNameUpperCamel};
-import ${basePackage}.service.${modelNameUpperCamel}Service;
+package com.company.project.web;
+import com.company.project.core.Result;
+import com.company.project.core.ResultGenerator;
+import com.company.project.model.User;
+import com.company.project.service.UserService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Controller;
@@ -16,53 +16,54 @@ import javax.annotation.Resource;
 import java.util.List;
 
 /**
-* Created by ${author} on ${date}.
+* Created by CodeGenerator on 2017/07/03.
 */
 @Controller
-@RequestMapping("${baseRequestMapping}")
-public class ${modelNameUpperCamel}Controller {
+@RequestMapping("/user")
+public class UserController {
     @Resource
-    private ${modelNameUpperCamel}Service ${modelNameLowerCamel}Service;
+    private UserService userService;
 
     @GetMapping("/manager")
     public String manager(Model model){
-        //...
-        return "${modelNameLowerCamel}-manager";
+        model.addAttribute("userList", userService.findAll());
+        model.addAttribute("title", "用户管理");
+        return "user-manager";
     }
 
     @ResponseBody
     @PostMapping("/add")
-    public Result add(${modelNameUpperCamel} ${modelNameLowerCamel}) {
-        ${modelNameLowerCamel}Service.save(${modelNameLowerCamel});
+    public Result add(User user) {
+        userService.save(user);
         return ResultGenerator.genSuccessResult();
     }
 
     @ResponseBody
     @PostMapping("/delete")
     public Result delete(Integer id) {
-        ${modelNameLowerCamel}Service.deleteById(id);
+        userService.deleteById(id);
         return ResultGenerator.genSuccessResult();
     }
 
     @ResponseBody
     @PostMapping("/update")
-    public Result update(${modelNameUpperCamel} ${modelNameLowerCamel}) {
-        ${modelNameLowerCamel}Service.update(${modelNameLowerCamel});
+    public Result update(User user) {
+        userService.update(user);
         return ResultGenerator.genSuccessResult();
     }
 
     @ResponseBody
     @PostMapping("/detail")
     public Result detail(Integer id) {
-        ${modelNameUpperCamel} ${modelNameLowerCamel} = ${modelNameLowerCamel}Service.findById(id);
-        return ResultGenerator.genSuccessResult(${modelNameLowerCamel});
+        User user = userService.findById(id);
+        return ResultGenerator.genSuccessResult(user);
     }
 
     @ResponseBody
     @PostMapping("/list")
     public Result list(Integer page, Integer size) {
         PageHelper.startPage(page, size);
-        List<${modelNameUpperCamel}> list = ${modelNameLowerCamel}Service.findAll();
+        List<User> list = userService.findAll();
         PageInfo pageInfo = new PageInfo(list);
         return ResultGenerator.genSuccessResult(pageInfo);
     }
