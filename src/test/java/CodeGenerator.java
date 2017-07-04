51235de2525a0f08f7eaa1e57387ae56a08ecfa0
luolina <luolina@zbj.com>
2017-07-04 09:29:12 +0800

    代码生成的修改

--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -16,9 +16,9 @@ import static com.company.project.core.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test";
+    private static final String JDBC_URL = "jdbc:mysql://222.177.55.60:3306/test";
     private static final String JDBC_USERNAME = "root";
-    private static final String JDBC_PASSWORD = "123456";
+    private static final String JDBC_PASSWORD = "!*fouryou2017";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
 
     private static final String PROJECT_PATH = System.getProperty("user.dir");//项目在硬盘上的基础路径
